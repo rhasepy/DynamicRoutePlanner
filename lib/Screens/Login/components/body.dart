8a85427eaa376a91d6b796faab6840040c4611a1
rhasepy <ozannyesiller@gmail.com>
2021-12-15 20:07:47 +0300

    Initial commit

---
+++ lib/Screens/Login/components/body.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:dynamicrouteplanner/Screens/Login/components/background.dart';
+import 'package:dynamicrouteplanner/Screens/Signup/signup_screen.dart';
+import 'package:dynamicrouteplanner/components/already_have_an_account_acheck.dart';
+import 'package:dynamicrouteplanner/components/rounded_button.dart';
+import 'package:dynamicrouteplanner/components/rounded_input_field.dart';
+import 'package:dynamicrouteplanner/components/rounded_password_field.dart';
+import 'package:flutter_svg/svg.dart';
+
+class Body extends StatelessWidget {
+  const Body({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    String email = "";
+    String password = "";
+
+    return Background(
+      child: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              "LOGIN",
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: size.height * 0.03),
+            SvgPicture.asset(
+              "assets/icons/login.svg",
+              height: size.height * 0.35,
+            ),
+            SizedBox(height: size.height * 0.03),
+            RoundedInputField(
+              hintText: "Your Email",
+              onChanged: (value) {
+                email = value;
+              },
+            ),
+            RoundedPasswordField(
+              onChanged: (value) {
+                password = value;
+              },
+            ),
+            RoundedButton(
+              text: "LOGIN",
+              press: () {
+                print(email);
+                print(password);
+              },
+            ),
+            SizedBox(height: size.height * 0.03),
+            AlreadyHaveAnAccountCheck(
+              press: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return SignUpScreen();
+                    },
+                  ),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
