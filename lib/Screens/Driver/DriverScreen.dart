7010827f9ad14f3833790117cad027f9497d64d8
rhasepy <ozannyesiller@gmail.com>
2021-12-28 22:39:35 +0300

    28.12.2021

--- lib/Screens/Driver/DriverScreen.dart
+++ lib/Screens/Driver/DriverScreen.dart
@@ -1,11 +1,77 @@
-import 'package:dynamicrouteplanner/Screens/Login/components/background.dart';
-import 'package:dynamicrouteplanner/components/rounded_button.dart';
 import 'package:flutter/material.dart';
-import 'package:dynamicrouteplanner/constants.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'dart:async';
 
 class Driver extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Google Maps Demo',
+      home: MapSample(),
+    );
+  }
+}
+
+class MapSample extends StatefulWidget {
+  @override
+  State<MapSample> createState() => MapSampleState();
+}
+
+class MapSampleState extends State<MapSample> {
+  Completer<GoogleMapController> _controller = Completer();
+
+  static final CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.4746,
+  );
+
+  static final CameraPosition _kLake = CameraPosition(
+      bearing: 192.8334901395799,
+      target: LatLng(37.43296265331129, -122.08832357078792),
+      tilt: 59.440717697143555,
+      zoom: 19.151926040649414);
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      body: GoogleMap(
+        mapType: MapType.hybrid,
+        initialCameraPosition: _kGooglePlex,
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+        },
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: _goToTheLake,
+        label: Text('To the lake!'),
+        icon: Icon(Icons.directions_boat),
+      ),
+    );
+  }
+
+  Future<void> _goToTheLake() async {
+    print("asd");
+    final GoogleMapController controller = await _controller.future;
+    controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
+  }
+}
+
+/*class Driver extends StatelessWidget {
+
+  static final CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.4746,
+  );
+
+  static final CameraPosition _kLake = CameraPosition(
+      bearing: 192.8334901395799,
+      target: LatLng(37.43296265331129, -122.08832357078792),
+      tilt: 59.440717697143555,
+      zoom: 19.151926040649414);
+
+  @override
+  Widget build(BuildContext context) {
+
     return Background(
       child: SingleChildScrollView(
         child: Column(
@@ -24,4 +90,4 @@ class Driver extends StatelessWidget {
       ),
     );
   }
-}
+}*/
--- lib/Screens/TypeRouter/TypeAskerAPIUI.dart
+++ lib/Screens/TypeRouter/TypeAskerAPIUI.dart
@@ -9,152 +9,150 @@ import 'package:dynamicrouteplanner/components/rounded_phone_field.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:dynamicrouteplanner/constants.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 
-class TypeAsker extends StatelessWidget
+class TypeAsker extends StatefulWidget {
+  @override
+  _TypeAsker createState() => _TypeAsker();
+}
+
+class _TypeAsker extends State<TypeAsker>
 {
   String _phoneNumber;
   String _nickName;
   DRPAuthAPI _API;
+  bool stayScreen;
 
-  TypeAsker() {
-      this._phoneNumber = "";
-      this._nickName = "";
-      this._API = DRPAuthAPI();
-      if (_isPassenger() != null) {
-        print("go to passenger");
+  _TypeAsker() {
+    this._phoneNumber = "";
+    this._nickName = "";
+    this._API = DRPAuthAPI();
+    this.stayScreen = false;
+  }
+
+  void tryApply(Map<String, dynamic> user) {
+    try {
+      if (user.containsKey("incomingList")) {
+        Navigator.pushAndRemoveUntil(context, MaterialPageRoute(builder: (context) => Driver()), ModalRoute.withName("/Home"));
       }
-      if (_isDriver() != null) {
-        print("go to driver");
+      else if (user.containsKey("incoming")) {
+        Navigator.pushAndRemoveUntil(context, MaterialPageRoute(builder: (context) => Passenger()), ModalRoute.withName("/Home"));
       }
-      
-      FirebaseFirestore.instance.collection('Drivers').doc("eYvBdhtz7UQiOQQRsNgiZWJDVYn1").snapshots().listen((event) {
-          print("ozannyesiller@gmail.com değişti");
+    } catch (e) {}
+  }
+
+  @override
+  void initState() {
+
+    WidgetsBinding.instance.addPostFrameCallback((timeStamp) async {
+      await FirebaseFirestore.instance.collection('Drivers').doc(FirebaseAuth.instance.currentUser.uid).get().then((value) =>
+          tryApply(value.data()));
+      setState(() {});
+    });
+
+    WidgetsBinding.instance.addPostFrameCallback((timeStamp) async {
+      await FirebaseFirestore.instance.collection('Passengers').doc(FirebaseAuth.instance.currentUser.uid).get().then((value) =>
+          tryApply(value.data()));
+      setState(() {
+        stayScreen = true;
       });
-  }
-
-  Future<bool> _isDriver() async {
-
-    bool returnVal = false;
-    await FirebaseFirestore.instance.collection('Drivers').doc(FirebaseAuth.instance.currentUser.uid).get().then((value) => {
-      if (value.data() == null) {
-        returnVal = false
-      } else {
-        returnVal = true
-      }
     });
-
-    if (returnVal) {
-      return true;
-    }
-    return null;
-  }
-
-  Future<bool> _isPassenger() async {
-
-    bool returnVal = false;
-    await FirebaseFirestore.instance.collection('Passengers').doc(FirebaseAuth.instance.currentUser.uid).get().then((value) => {
-      if (value.data() == null) {
-        returnVal = false
-      } else {
-        returnVal = true
-      }
-    });
-
-    if (returnVal) {
-      return true;
-    }
-    return null;
   }
 
   @override
   Widget build(BuildContext context) {
-      return Scaffold(
-          backgroundColor: Colors.black,
-          body: Background(
-            child: SingleChildScrollView(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  RoundedInputField(
-                    hintText: "Your Nickname",
-                    onChanged: (value) {
-                      _nickName = value;
-                    },
-                  ),
-                  RoundedPhoneField(
-                    onChanged: (value) {
-                      _phoneNumber = value;
-                    },
-                  ),
-                  RoundedButton(
-                    text: "Passenger",
-                    press: () async {
-                      if (_nickName == "" || _phoneNumber == "") {
-                        Fluttertoast.showToast(
-                            msg: "Name or phone label can not be empty",
-                            toastLength: Toast.LENGTH_SHORT,
-                            gravity: ToastGravity.BOTTOM,
-                            timeInSecForIosWeb: 1,
-                            backgroundColor: Colors.red,
-                            textColor: Colors.white,
-                            fontSize: 16.0
-                        );
-                      } else {
-                        await _API.addPassenger(
-                            {
-                              "name" : this._nickName,
-                              "phone" : this._phoneNumber,
-                              "lat" : 0.toDouble(),
-                              "lng" : 0.toDouble(),
-                              "incoming" : false,
-                              "busID" : 0,
-                              "UUID" : FirebaseAuth.instance.currentUser.uid,
-                              "email" : FirebaseAuth.instance.currentUser.email
-                            }
-                        );
-                        Navigator.pushAndRemoveUntil(context, MaterialPageRoute(builder: (context) => Passenger()), ModalRoute.withName("/Home"));
-                      }
-                    },
-                  ),
-                  RoundedButton(
-                    text: "Driver",
-                    color: kPrimaryLightColor,
-                    textColor: Colors.black,
-                    press: () async {
-                      if (_nickName == "" || _phoneNumber == "") {
-                        Fluttertoast.showToast(
-                            msg: "Name or phone label can not be empty",
-                            toastLength: Toast.LENGTH_SHORT,
-                            gravity: ToastGravity.BOTTOM,
-                            timeInSecForIosWeb: 1,
-                            backgroundColor: Colors.red,
-                            textColor: Colors.white,
-                            fontSize: 16.0
-                        );
-                      } else {
-                        List emptyList = List.empty();
-                        await _API.addDriver(
-                            {
-                              "name" : this._nickName,
-                              "phone" : this._phoneNumber,
-                              "lat" : 0.toDouble(),
-                              "lng" : 0.toDouble(),
-                              "incomingList" : emptyList,
-                              "busID" : 0,
-                              "UUID" : FirebaseAuth.instance.currentUser.uid,
-                              "email" : FirebaseAuth.instance.currentUser.email
-                            }
-                        );
-                        Navigator.pushAndRemoveUntil(context, MaterialPageRoute(builder: (context) => Driver()), ModalRoute.withName("/Home"));
-                      }
-                    },
-                  ),
-                ],
+
+      if (stayScreen == false) {
+        return Scaffold();
+      } else {
+        return Scaffold(
+            backgroundColor: Colors.black,
+            body: Background(
+              child: SingleChildScrollView(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    RoundedInputField(
+                      hintText: "Your Nickname",
+                      onChanged: (value) {
+                        _nickName = value;
+                      },
+                    ),
+                    RoundedPhoneField(
+                      onChanged: (value) {
+                        _phoneNumber = value;
+                      },
+                    ),
+                    RoundedButton(
+                      text: "Passenger",
+                      press: () async {
+                        if (_nickName == "" || _phoneNumber == "") {
+                          Fluttertoast.showToast(
+                              msg: "Name or phone label can not be empty",
+                              toastLength: Toast.LENGTH_SHORT,
+                              gravity: ToastGravity.BOTTOM,
+                              timeInSecForIosWeb: 1,
+                              backgroundColor: Colors.red,
+                              textColor: Colors.white,
+                              fontSize: 16.0
+                          );
+                        } else {
+                          await _API.addPassenger(
+                              {
+                                "name" : this._nickName,
+                                "phone" : this._phoneNumber,
+                                "lat" : 0.toDouble(),
+                                "lng" : 0.toDouble(),
+                                "incoming" : false,
+                                "busID" : 0,
+                                "UUID" : FirebaseAuth.instance.currentUser.uid,
+                                "email" : FirebaseAuth.instance.currentUser.email
+                              }
+                          );
+                          Navigator.pushAndRemoveUntil(context, MaterialPageRoute(builder: (context) => Passenger()), ModalRoute.withName("/Home"));
+                        }
+                      },
+                    ),
+                    RoundedButton(
+                      text: "Driver",
+                      color: kPrimaryLightColor,
+                      textColor: Colors.black,
+                      press: () async {
+                        if (_nickName == "" || _phoneNumber == "") {
+                          Fluttertoast.showToast(
+                              msg: "Name or phone label can not be empty",
+                              toastLength: Toast.LENGTH_SHORT,
+                              gravity: ToastGravity.BOTTOM,
+                              timeInSecForIosWeb: 1,
+                              backgroundColor: Colors.red,
+                              textColor: Colors.white,
+                              fontSize: 16.0
+                          );
+                        } else {
+                          List emptyList = List.empty();
+                          await _API.addDriver(
+                              {
+                                "name" : this._nickName,
+                                "phone" : this._phoneNumber,
+                                "lat" : 0.toDouble(),
+                                "lng" : 0.toDouble(),
+                                "incomingList" : emptyList,
+                                "busID" : 0,
+                                "UUID" : FirebaseAuth.instance.currentUser.uid,
+                                "email" : FirebaseAuth.instance.currentUser.email
+                              }
+                          );
+                          Navigator.pushAndRemoveUntil(context, MaterialPageRoute(builder: (context) => Driver()), ModalRoute.withName("/Home"));
+                        }
+                      },
+                    ),
+                  ],
+                ),
               ),
-            ),
-          )
-      );
+            )
+        );
+      }
   }
 }
